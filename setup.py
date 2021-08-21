5b713549cc7cdb254cfd403f2998eaf80491c9a6
Stefan Helmert <stefan.helmert@t-online.de>
2021-08-21 18:31:05 +0200

    version 0.0.1 with githook; available in pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import setuptools
+import xyzcad
 
 
 with open("README.md", "r") as fh:
@@ -8,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xyzcad",
-    version=cryptdomainmgr.__version__,
+    version=xyzcad.__version__,
     author="Stefan Helmert",
     author_email="stefan.helmert@t-online.de",
     description="Software renders f(x,y,z) into a printable STL.",
@@ -16,11 +17,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TheTesla/xyzcad",
     packages=setuptools.find_packages(exclude=['test*']),
-    include_package_data=True,
+    #include_package_data=True,
     install_requires=[
         "numpy",
         "numba",
-        "GitPython",
         "open3d"
     ],
     license = 'https://www.fsf.org/licensing/licenses/agpl-3.0.html',

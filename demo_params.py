2869d7ad6fe47061cfb168ae0ba1376fa23dd5fd
Stefan Helmert <stefan.helmert@t-online.de>
2025-04-18 20:18:29 +0200

    better TLT handling

---
+++ demo_params.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#######################################################################
+#
+#    xyzCad - functional cad software for 3d printing
+#    Copyright (c) 2021 Stefan Helmert <stefan.helmert@t-online.de>
+#
+#######################################################################
+
+
+import math
+import time
+
+from numba import jit, njit
+
+from xyzcad import render
+
+
+@njit
+def sphere(x, y, z):
+    return r**2 > x**2 + y**2 + z**2
+
+
+t0 = time.time()
+
+
+render.renderAndSave(sphere, "demo_params.stl", 1)
+
+print(time.time() - t0)
